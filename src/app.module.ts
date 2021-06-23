328a41af1491d25c2c5629a9482dc875faf79921
Kichiyaki <xyztojajestem@gmail.com>
2021-06-23 13:07:17 +0200

    add a new module - characters

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CharactersModule } from './characters/characters.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.MONGODB_URI ?? 'mongodb://localhost/star-wars',
     ),
+    CharactersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/characters/character.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Character {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  episodes: string[];
+
+  @Prop()
+  planet?: string;
+}
+
+export type CharacterDocument = Character & Document;
+
+export const CharacterSchema = SchemaFactory.createForClass(Character);
